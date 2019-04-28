1726f7f31c143db7eb0b631ca88d4792f0756183
Elliot Pak <elliot@elliotpak.com>
2019-04-28 12:57:20 +0800

    minor rewrite to loading and decoding

--- src/Hssb/Layout.hs
+++ src/Hssb/Layout.hs
@@ -10,12 +10,14 @@ loadEntryFile :: Macro -> FilePath -> IO (MacroResult)
 loadEntryFile macro path = do
     exists <- doesFileExist path
     if (not exists) then return $ Left $ InvalidLayoutFile path
-    else decodeEntryFile macro path
+    else do 
+        contents <- readFile path
+        let doc = decodeEntryFile macro path contents
+        return doc
 
-decodeEntryFile :: Macro -> FilePath -> IO (MacroResult)
-decodeEntryFile macro path = do
-    contents <- readFile path
+decodeEntryFile :: Macro -> FilePath -> String -> MacroResult
+decodeEntryFile macro path contents = do
     let layout = Y.decodeEither' $ B.pack contents :: Either Y.ParseException Value
-    return $ case layout of
+    case layout of
       Right (Object params) -> macro params
       otherwise             -> Left $ InvalidLayoutFile path

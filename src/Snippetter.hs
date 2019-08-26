060b388ad16ee9e9a580b033e6a6500411b5395c
Elliot Pak <elliot@elliotpak.com>
2019-08-26 17:00:30 +0800

    remove old source files that arent needed

--- src/Snippetter.hs
+++
@@ -1,8 +0,0 @@
-module Snippetter where
-
-import Control.Monad.Trans.Except
-import Data.HashMap.Strict
-import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
-import Snippetter.Build
-import Snippetter.Layout

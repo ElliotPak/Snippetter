644e212721f67e4c3760f3e80796ab7d3712322b
Elliot Pak <elliot@elliotpak.com>
2019-09-12 15:20:59 +0800

    naive layout file updating

--- src/Snippetter/DepManager.hs
+++ src/Snippetter/DepManager.hs
@@ -13,12 +13,14 @@ import Snippetter.Utilities
 data DepManError
   = LayoutDepManError LayoutError
   | FileDepManError FileError
+  | GraphDepManError FG.GraphError
   | CyclicFileGraph [FG.SCComponent]
   deriving (Eq)
 
 instance Show DepManError where
   show (LayoutDepManError e) = show e
   show (FileDepManError e) = show e
+  show (GraphDepManError e) = show e
   show (CyclicFileGraph g) =
     "The following cyclic dependencies were found: \n" <>
     T.unpack (FG.showSCC g)
@@ -44,6 +46,33 @@ addDependencies sa graph = do
     case saOutputFile sa of
       Nothing -> FG.addFiles deps graph
       Just a -> FG.addChild a deps graph
+
+-- | Check if a @SiteAction@ should be updated. This is the case when its
+-- output file is not up to date but its dependencies are.
+shouldUpdateSiteAction ::
+     MonadReadWorld m => FG.FileGraph -> SiteAction -> DepManResult m Bool
+shouldUpdateSiteAction graph sa = do
+  outputUpToDate <-
+    case saOutputFile sa of
+      Just a -> FG.isUpToDate a graph `mapResultError` GraphDepManError
+      Nothing -> return True
+  deps <- saNeededFiles sa `mapResultError` LayoutDepManError
+  depsUpToDate <- FG.areUpToDate deps graph `mapResultError` GraphDepManError
+  return (not outputUpToDate && depsUpToDate)
+
+-- | Evaluate a @SiteAction@ if it's not up to date and its dependencies are.
+updateSiteAction ::
+     MonadWriteWorld m => FG.FileGraph -> SiteAction -> DepManResult m ()
+updateSiteAction graph sa = do
+  shouldUpdate <- shouldUpdateSiteAction graph sa
+  when shouldUpdate (resultLift $ executeSiteAction sa)
+
+-- | Update all @SiteAction@s resulting from a layout file.
+updateLayoutFile ::
+     MonadWriteWorld m => FilePath -> BuilderMap -> DepManResult m ()
+updateLayoutFile layoutFile map = do
+  depInfo <- getDepInfo layoutFile map
+  mapM_ (updateSiteAction $ graph depInfo) (actions depInfo)
 
 -- | Builds dependency information from all layout files in a directory
 -- (recursively).
